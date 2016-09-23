34d8d18fdcb4765749d5e23228516dcbbadbc51f
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-23 12:31:44 +0200

    Comment out WIP.

--- WpfAnalyzers.Analyzers/DependencyProperties/WA1200DependencyPropertyFieldMustBeNamedNameProperty.cs
+++ WpfAnalyzers.Analyzers/DependencyProperties/WA1200DependencyPropertyFieldMustBeNamedNameProperty.cs
@@ -1,54 +1,54 @@
-﻿namespace WpfAnalyzers.DependencyProperties
-{
-    using System.Collections.Immutable;
-    using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
-    using Microsoft.CodeAnalysis.Diagnostics;
+﻿//namespace WpfAnalyzers.DependencyProperties
+//{
+//    using System.Collections.Immutable;
+//    using Microsoft.CodeAnalysis;
+//    using Microsoft.CodeAnalysis.CSharp;
+//    using Microsoft.CodeAnalysis.Diagnostics;
 
-    using WpfAnalyzers.DependencyProperties.Internals;
+//    using WpfAnalyzers.DependencyProperties.Internals;
 
-    /// <summary>
-    /// DependencyProperty field must be named &lt;Name&gt;Property
-    /// </summary>
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    internal class WA1200DependencyPropertyFieldMustBeNamedNameProperty : DiagnosticAnalyzer
-    {
-        public const string DiagnosticId = "WA1200";
-        private const string Title = "DependencyProperty field must be named <Name>Property";
-        private const string MessageFormat = "DependencyProperty '{0}' field must be named <Name>Property";
-        private const string Description = "DependencyProperty field must be named <Name>Property";
-        private const string HelpLink = "http://stackoverflow.com/";
+//    /// <summary>
+//    /// DependencyProperty field must be named &lt;Name&gt;Property
+//    /// </summary>
+//    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+//    internal class WA1200DependencyPropertyFieldMustBeNamedNameProperty : DiagnosticAnalyzer
+//    {
+//        public const string DiagnosticId = "WA1200";
+//        private const string Title = "DependencyProperty field must be named <Name>Property";
+//        private const string MessageFormat = "DependencyProperty '{0}' field must be named <Name>Property";
+//        private const string Description = "DependencyProperty field must be named <Name>Property";
+//        private const string HelpLink = "http://stackoverflow.com/";
 
-        private static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
-            DiagnosticId,
-            Title,
-            MessageFormat,
-            AnalyzerCategory.DependencyProperties,
-            DiagnosticSeverity.Warning,
-            AnalyzerConstants.EnabledByDefault,
-            Description,
-            HelpLink);
+//        private static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
+//            DiagnosticId,
+//            Title,
+//            MessageFormat,
+//            AnalyzerCategory.DependencyProperties,
+//            DiagnosticSeverity.Warning,
+//            AnalyzerConstants.EnabledByDefault,
+//            Description,
+//            HelpLink);
 
-        /// <inheritdoc/>
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptor);
+//        /// <inheritdoc/>
+//        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptor);
 
-        /// <inheritdoc/>
-        public override void Initialize(AnalysisContext context)
-        {
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-            context.EnableConcurrentExecution();
-            context.RegisterSyntaxNodeAction(HandleFieldDeclaration, SyntaxKind.FieldDeclaration);
-        }
+//        /// <inheritdoc/>
+//        public override void Initialize(AnalysisContext context)
+//        {
+//            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+//            context.EnableConcurrentExecution();
+//            context.RegisterSyntaxNodeAction(HandleFieldDeclaration, SyntaxKind.FieldDeclaration);
+//        }
 
-        private static void HandleFieldDeclaration(SyntaxNodeAnalysisContext context)
-        {
-            var fieldSymbol = (IFieldSymbol)context.ContainingSymbol;
-            if (!fieldSymbol.IsDependencyPropertyField())
-            {
-                return;
-            }
+//        private static void HandleFieldDeclaration(SyntaxNodeAnalysisContext context)
+//        {
+//            var fieldSymbol = (IFieldSymbol)context.ContainingSymbol;
+//            if (!fieldSymbol.IsDependencyPropertyField())
+//            {
+//                return;
+//            }
 
-            //context.ReportDiagnostic(Diagnostic.Create(Descriptor, context.Node.GetLocation(), fieldSymbol.Name));
-        }
-    }
-}
+//            //context.ReportDiagnostic(Diagnostic.Create(Descriptor, context.Node.GetLocation(), fieldSymbol.Name));
+//        }
+//    }
+//}
--- WpfAnalyzers.Test/DependencyProperties/WA1200FieldMustBeNamedNamePropertyTests.cs
+++ WpfAnalyzers.Test/DependencyProperties/WA1200FieldMustBeNamedNamePropertyTests.cs
@@ -1,86 +1,86 @@
-﻿namespace WpfAnalyzers.Test.DependencyProperties
-{
-    using System.Collections.Generic;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Microsoft.CodeAnalysis.CodeFixes;
-    using Microsoft.CodeAnalysis.Diagnostics;
-    using NUnit.Framework;
-    using WpfAnalyzers.DependencyProperties;
+﻿//namespace WpfAnalyzers.Test.DependencyProperties
+//{
+//    using System.Collections.Generic;
+//    using System.Threading;
+//    using System.Threading.Tasks;
+//    using Microsoft.CodeAnalysis.CodeFixes;
+//    using Microsoft.CodeAnalysis.Diagnostics;
+//    using NUnit.Framework;
+//    using WpfAnalyzers.DependencyProperties;
 
-    public class WA1200FieldMustBeNamedNamePropertyUnitTests : CodeFixVerifier
-    {
-        [Test]
-        public async Task HappyPath()
-        {
-            var testCode = @"
-    using System.Windows;
-    using System.Windows.Controls;
+//    public class WA1200FieldMustBeNamedNamePropertyUnitTests : CodeFixVerifier
+//    {
+//        [Test]
+//        public async Task HappyPath()
+//        {
+//            var testCode = @"
+//    using System.Windows;
+//    using System.Windows.Controls;
 
-    public class FooControl : Control
-    {
-        public static readonly DependencyProperty BarProperty = DependencyProperty.Register(
-            ""Bar"", typeof(int), typeof(FooControl), new PropertyMetadata(default(int)));
+//    public class FooControl : Control
+//    {
+//        public static readonly DependencyProperty BarProperty = DependencyProperty.Register(
+//            ""Bar"", typeof(int), typeof(FooControl), new PropertyMetadata(default(int)));
 
-        public int Bar
-        {
-            get { return (int) GetValue(BarProperty); }
-            set { SetValue(BarProperty, value); }
-        }
-    }";
+//        public int Bar
+//        {
+//            get { return (int) GetValue(BarProperty); }
+//            set { SetValue(BarProperty, value); }
+//        }
+//    }";
 
-            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
-        }
+//            await this.VerifyCSharpDiagnosticAsync(testCode, EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+//        }
 
-        [Test]
-        public async Task PropertyNotNamedNameProperty()
-        {
-            var testCode = @"
-    using System.Windows;
-    using System.Windows.Controls;
+//        [Test]
+//        public async Task PropertyNotNamedNameProperty()
+//        {
+//            var testCode = @"
+//    using System.Windows;
+//    using System.Windows.Controls;
 
-    public class FooControl : Control
-    {
-        public static readonly DependencyProperty BarProperty = DependencyProperty.Register(
-            ""Error"", typeof(int), typeof(FooControl), new PropertyMetadata(default(int)));
+//    public class FooControl : Control
+//    {
+//        public static readonly DependencyProperty BarProperty = DependencyProperty.Register(
+//            ""Error"", typeof(int), typeof(FooControl), new PropertyMetadata(default(int)));
 
-        public int Bar
-        {
-            get { return (int) GetValue(BarProperty); }
-            set { SetValue(BarProperty, value); }
-        }
-    }";
+//        public int Bar
+//        {
+//            get { return (int) GetValue(BarProperty); }
+//            set { SetValue(BarProperty, value); }
+//        }
+//    }";
 
-            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(3, 30);
+//            DiagnosticResult expected = this.CSharpDiagnostic().WithLocation(3, 30);
 
-            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+//            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
 
-            var fixedCode = @"
-    using System.Windows;
-    using System.Windows.Controls;
+//            var fixedCode = @"
+//    using System.Windows;
+//    using System.Windows.Controls;
 
-    public class FooControl : Control
-    {
-        public static readonly DependencyProperty ErrorProperty = DependencyProperty.Register(
-            ""Error"", typeof(int), typeof(FooControl), new PropertyMetadata(default(int)));
+//    public class FooControl : Control
+//    {
+//        public static readonly DependencyProperty ErrorProperty = DependencyProperty.Register(
+//            ""Error"", typeof(int), typeof(FooControl), new PropertyMetadata(default(int)));
 
-        public int Bar
-        {
-            get { return (int) GetValue(ErrorProperty); }
-            set { SetValue(ErrorProperty, value); }
-        }
-    }";
-            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
-        }
+//        public int Bar
+//        {
+//            get { return (int) GetValue(ErrorProperty); }
+//            set { SetValue(ErrorProperty, value); }
+//        }
+//    }";
+//            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+//        }
 
-        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
-        {
-            yield return new WA1200DependencyPropertyFieldMustBeNamedNameProperty();
-        }
+//        protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
+//        {
+//            yield return new WA1200DependencyPropertyFieldMustBeNamedNameProperty();
+//        }
 
-        protected override CodeFixProvider GetCSharpCodeFixProvider()
-        {
-            return new RenameDependencyPropertyFieldCodeFixProvider();
-        }
-    }
-}
+//        protected override CodeFixProvider GetCSharpCodeFixProvider()
+//        {
+//            return new RenameDependencyPropertyFieldCodeFixProvider();
+//        }
+//    }
+//}
